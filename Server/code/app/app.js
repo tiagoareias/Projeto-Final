6a1212b120205263b9e351b977bbcadca4ae99bc
Tiago Areias <aluno19998@ipt.pt>
2019-04-07 23:06:33 +0100

    CRUD para os Utilizadores

--- Server/code/app/app.js
+++ Server/code/app/app.js
@@ -3,12 +3,68 @@ const express = require('express');
 //criação da api
 const app = express();
 const router = express.Router();
+const db = require('../config/database')
+const {User} = require('../app/models')
+const bodyParser = require('body-parser');
 
-/**PÁGINA PRINCIPAL */
+app.use(bodyParser.urlencoded({extended:false}));
 
-const index = router.get('/', (req,res,next) =>{
-   res.send("Projeto Final: Portal para interação com serviço de reconhecimento emocional em música");
+/**********PÁGINA PRINCIPAL **********/
+
+app.get('/',async (req,res) =>{
+    
+    res.status(201).send("Página Inicial");
+  
 });
 
-app.use('/',index);
+/**********UTILIZADORES **********/
+
+//obter todos os utilizadores da base de dados
+
+app.get('/api/users',async (req,res) =>{
+    User.findAll().then(users =>{
+       res.status(201).send(users);
+    })
+    .catch(err => console.log(err));
+});
+
+
+//criar novo utilizador
+app.post('/api/users/create', async (req, res) => {
+    const user = await User.create(req.body);
+    res.json(user);
+  });
+
+
+//obter utilizador pelo id
+app.get('/api/users/:id', async (req,res) => {
+    let id = req.params.id;
+    User.findByPk(id).then(user =>{
+        res.status(201).send(user);
+    })
+    .catch(err => console.log(err));
+});
+
+//editar dados do utilizador
+
+//editar utilizador
+app.post('/api/users/:id/edit', async (req, res) => {
+    let id = req.params.id;
+    User.findByPk(id).then(user =>{
+        const userEdited = user.update(req.body);
+        res.json(user);
+    })
+    .catch(err => console.log(err));
+  });
+
+//apagar utilizador
+app.post('/api/users/:id/delete', async (req,res) => {
+    let id = req.params.id;
+    User.findByPk(id).then(user =>{
+        const userDeleted = user.destroy();
+        res.json("Utilizador Eliminado");
+    })
+    .catch(err => console.log(err));
+})
+
 module.exports = app;
--- Server/code/server.js
+++ Server/code/server.js
@@ -1,16 +1,11 @@
 
 const app = require('../code/app/app');       
 const http = require('http');
-const bodyParser = require('body-parser');
+
 const {User} = require('../code/app/models')
 
 const port = 5000; 
 app.set('port',port);
-
-app.use(bodyParser.urlencoded({extended:false}));
-User.create({firstName: "Tiago", lastName: "Areias", email:"areias@mail.pt"})
-//Users.create(null);
-//Users.create({firstName:"Tiago", lastName:"Areias", email:"areias@mail.pt"});
 
 
 //criação do servidor
