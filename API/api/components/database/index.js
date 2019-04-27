c5090556b8fca24d4297898ac119a3d48cadf30b
Tiago António <aluno20000@ipt.pt>
2019-04-28 00:08:08 +0100

    Utilização de vagrant
    Utilização de vagrant para a conexão com a base de dados.

--- API/api/components/database/index.js
+++ API/api/components/database/index.js
@@ -1,8 +1,9 @@
 var Sequelize = require('sequelize')
-var sequelize = new Sequelize('database_projetoFinal','root','root', {
-    host: 'localhost',
+var sequelize = new Sequelize('database_projetoFinal','user','123qwe', {
+    host: '127.0.0.1',
     dialect: 'mysql',
-    timezone:"+01:00"
+    timezone:"+01:00",
+    port:4000
 })
 
 const db = {
