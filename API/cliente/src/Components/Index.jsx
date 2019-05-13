eea1d5274b27e70c59fcfe5fedd474db99f22f40
Tiago Areias <aluno19998@ipt.pt>
2019-05-13 15:44:42 +0100

    Atualização ficheiros - front-end

---
+++ API/cliente/src/Components/Index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import '../CssComponents/index.css'
+class Index extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1 className="title">MER - PÁGINA PRINCIPAL</h1>
+      </div>
+      );
+  }
+}
+
+export default Index;
--- API/cliente/src/index.js
+++ API/cliente/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import IndexPage from '../src/MasterComponents/IndexPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={IndexPage} />
+        </Switch>
+    </ BrowserRouter>
+    , document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
