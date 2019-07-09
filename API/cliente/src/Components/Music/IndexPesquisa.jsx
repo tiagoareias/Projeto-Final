f39c35113c74d7e70727550e54ca416f77688da7
Tiago Areias <aluno19998@ipt.pt>
2019-07-10 00:17:49 +0100

    Loading Gif na Pesquisa
    Loading Gif na Pesquisa

--- API/cliente/src/Components/Music/IndexPesquisa.jsx
+++ API/cliente/src/Components/Music/IndexPesquisa.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import '../../CssComponents/index.css'
+import LoadingGif from '../Global/LoadingGif';
 class IndexPesquisa extends Component {
 
   constructor() {
@@ -8,7 +9,9 @@ class IndexPesquisa extends Component {
       alertText: "Ocorreu um erro técnico. Tente novamente mais tarde",
       alertisNotVisible: true,
       alertColor: "danger",
-      dataGet: []
+      dataGet: [],
+      isHidden: false
+
     }
   }
 
@@ -18,21 +21,28 @@ class IndexPesquisa extends Component {
     //this.setState({ dataGet: window.history.state.response })
   }
 
- async pesquisaMusica(pesquisaARealizar) {
+  async pesquisaMusica(pesquisaARealizar) {
     console.log(pesquisaARealizar)
     const response = await fetch(`http://localhost:8000/music/search/result/${pesquisaARealizar}`, {
       method: 'GET',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
       },
-  });
-  //Aguardar API
-  await response.json().then(resp => {
-    console.log(resp.response)
-      //Verificar o estado da resposta da API
-      this.setState({ dataGet: resp.response })
-      
-  });
+    });
+    //Aguardar API
+    await response.json().then(resp => {
+      if (resp.response.length !== 0) {
+        //Verificar o estado da resposta da API
+        this.setState({ dataGet: resp.response })
+        this.setState({ isHidden: true })
+      }
+      else {
+        this.setState({ isHidden: true })
+        this.setState({ dataGet: "vazio" })
+      }
+
+
+    });
   }
 
   eliminarMusica = async e => {
@@ -81,44 +91,59 @@ class IndexPesquisa extends Component {
               <hr />
             </div>
 
-            {
-              this.state.dataGet.map((data, index) => {
-                return (
+            {(this.state.dataGet === "vazio") ? (
+            <div className="col-md-12 mb-3">
+            <h3 className="display-5 text-center">Não foram encontrados resultados para {this.props.query}</h3>
+            
+                </div>             
+                 ) : (
+                <div>
+                  {
+                    this.state.dataGet.map((data, index) => {
+                      return (
 
-                  <div key={index} className="row">
-                    <div>
-                      <iframe id="frame" src={"https://www.youtube.com/embed/" + data.idVideo}
-                        title={data.name} autoPlay allowFullScreen></iframe>
-                    </div>
-                    <div>
-                      <div className="col-md-12 mb-3" id="frame">
-                        <h4><u>Autor</u>: {data.autor}</h4>
-                        <br />
-                        <h5 className="font-weight-bold ">{data.name}</h5>
-                        <br />
-                        <div className="text-secondary" >
-                          <h6 id="likes"> <i className="fa fa-thumbs-o-up"></i> <i>{data.numLikes}</i></h6>
-                          <h6 id="likes"> <i className="fa fa-thumbs-o-down"></i> <i >{data.numDislikes}</i></h6>
+                        <div key={index} className="row">
+                          <div>
+                            <iframe id="frame" src={"https://www.youtube.com/embed/" + data.idVideo}
+                              title={data.nome} autoPlay allowFullScreen></iframe>
+                          </div>
+                          <div>
+                            <div className="col-md-12 mb-3" id="frame">
+                              <h4><u>Autor</u>: {data.autor}</h4>
+                              <br />
+                              <h5 className="font-weight-bold ">{data.nome}</h5>
+                              <br />
+                              <div className="text-secondary" >
+                                <h6 id="likes"> <i className="fa fa-thumbs-o-up"></i> <i>{data.numLikes}</i></h6>
+                                <h6 id="likes"> <i className="fa fa-thumbs-o-down"></i> <i >{data.numDislikes}</i></h6>
+                              </div>
+                              <br />
+                              <h6 className="text-secondary"><i >{data.numViews}</i> Visualizações </h6>
+                              <h6 className="text-secondary"> Publicado a <i > {data.dataPublicacao.substring(0, 10)}</i></h6>
+                              {/*EMOCAO*/}
+                              {/*<h5 className="font-weight-bold "> Emoção: <i > {data.emocao} </i></h5>*/}
+                              <h5 className="font-weight-bold "> Emoção: <i className="text-secondary"> Emotion </i></h5>
+                              {/*Botão Eliminar*/}
+                              {(sessionStorage.getItem('token') != null) ? (
+                                <button id={data.idVideo} type="button" className="btn btn-danger" onClick={this.eliminarMusica} >Eliminar</button>
+                              ) : (<p></p>)}
+                            </div>
+                          </div>
                         </div>
-                        <br />
-                        <h6 className="text-secondary"><i >{data.numViews}</i> Visualizações </h6>
-                        <h6 className="text-secondary"> Publicado a <i > {data.dataPublicacao.substring(0, 10)}</i></h6>
-                      {/*EMOCAO*/}
-                      {/*<h5 className="font-weight-bold "> Emoção: <i > {data.emocao} </i></h5>*/}
-                      <h5 className="font-weight-bold "> Emoção: <i className="text-secondary"> Emotion </i></h5>
-                        {/*Botão Eliminar*/}
-                        {(sessionStorage.getItem('token') != null) ? (
-                          <button id={data.idVideo} type="button" className="btn btn-danger" onClick={this.eliminarMusica} >Eliminar</button>
-                        ) : (<p></p>)}
-                      </div>
-                    </div>
-                  </div>
-                )
-              })
+                      )
+                    })
 
 
-            }
+                  }
+                </div>
+              )}
+
           </div>
+          <center>
+            <LoadingGif
+              loading={this.state.isHidden}
+            />
+          </center>
           <br />
         </div>
       </div>
