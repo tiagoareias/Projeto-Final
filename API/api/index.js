8ba49a1717e323a5f306a75e1e5386a6e17d4378
Tiago António <aluno20000@ipt.pt>
2019-07-12 19:43:40 +0100

    alteracoes do static public/index.html
    alteracoes do static public/index.html

--- API/api/index.js
+++ API/api/index.js
@@ -1,7 +1,7 @@
-var models = require ('./components/models');
+var models = require('./components/models');
 
 var swaggerUi = require('swagger-ui-express')
-var swaggerDocument = require( './swagger/swagger.json');
+var swaggerDocument = require('./swagger/swagger.json');
 var expressValidator = require('express-validator');
 
 var express = require('express'),
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(expressValidator());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use('/api/doc',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -30,13 +30,15 @@ require('./components/music/index')(app)
 require('./components/feedback/index')(app)
 require('./components/refreshToken/index')(app)
 
+
+
 var port = process.env.PORT || 8000;
 models.sequelize.sync({}).then(() => {
     app.listen(port, () => {
         console.log('\x1b[32m%s %d\x1b[0m.', 'Server HTTP listening on port', port)
-    })  });
-
+    })
+});
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '../cliente/public/index.html')
 })
