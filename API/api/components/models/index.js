cc8a15ecda36016cda8d4d34b5417b2328c19729
Tiago António <aluno20000@ipt.pt>
2019-07-12 17:50:29 +0100

    Conexão com base de dados do servidor
    Conexão com base de dados do servidor

--- API/api/components/models/index.js
+++ API/api/components/models/index.js
@@ -1,10 +1,13 @@
 const Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('database_projetoFinal','user','123qwe', {
-  host: '127.0.0.1',
-  dialect: 'mysql',
+var sequelize = new Sequelize('d26jd4h2uf79uj','rhmnaceoelvzyc','125b87feeb6607bb08a6e1cd3e2f53a65e8f26ab6549f684468fe16872442b52', {
+  host: 'ec2-174-129-229-106.compute-1.amazonaws.com',
+  dialect: 'postgres',
   timezone:"+01:00",
-  port:4000,
+  port:5432,
+  dialectOptions: {
+    "ssl": true
+  },
   define: {
       charset: 'utf8',
       collate: 'utf8_general_ci', 
